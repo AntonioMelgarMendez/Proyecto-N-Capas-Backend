fc028a39b5450da9069461cbd6d9d922cb6fc48d
AntonioMelgarMendez <00009123@uca.edu.sv>
2026-06-06 19:05:51 -0600

    feature(stripe basic integration): adding payment end points

--- src/main/java/com/proyecto/proyectoncapas/services/payment/PaymentService.java
+++ src/main/java/com/proyecto/proyectoncapas/services/payment/PaymentService.java
@@ -3,4 +3,6 @@ import com.stripe.model.Event;
 public interface PaymentService {
     String startPaymentReservation(Long reservationId);
     void confirmPayment(Event event);
+    void refundSecurityDeposit(Long reservationId);
+    String getPaymentStatus(Long reservationId);
 }
